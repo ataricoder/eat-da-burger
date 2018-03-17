1c4fbd4460ba681db02d7a90f90b925917025d68
danysexymexy <danysexymexy@utexas.edu>
2018-03-16 22:13:19 -0500

    1

---
+++ controllers/burgersController.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+    console.log(result);
+    res.sendStatus(200);
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
